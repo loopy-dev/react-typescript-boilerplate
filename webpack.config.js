1eb0c4fce9f15111678344d76f1238558a923cf5
Junhyuck Ko <mrbartrns@naver.com>
2022-05-25 10:57:35 +0900

    chore: postcss settings

--- webpack.config.js
+++ webpack.config.js
@@ -33,7 +33,7 @@ module.exports = {
       },
       {
         test: /\.s?css$/,
-        use: ['style-loader', 'css-loader', 'sass-loader'],
+        use: ['style-loader', 'css-loader', 'postcss-loader', 'sass-loader'],
       },
     ],
   },
