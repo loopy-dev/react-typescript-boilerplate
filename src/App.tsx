ba1d6ce380aff52964d009600987ec868f3ca717
Junhyuck Ko <mrbartrns@naver.com>
2022-05-30 10:19:15 +0900

    [chore] update lint settings

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 
-function App(): JSX.Element {
+const App: React.FC = () => {
   return (
     <div className="App">
       <h1>Hello, World!</h1>
     </div>
   );
-}
+};
 
 export default App;
