336f83ff6e373604d152003f3757cf6a58c5635f
Junhyuck Ko <mrbartrns@naver.com>
2022-11-03 21:26:57 +0900

    chore: update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,67 +1,118 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
 const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
 
-module.exports = {
-  resolve: {
-    extensions: ['.js', '.jsx', '.ts', '.tsx'],
-    alias: {
-      '@': path.resolve(__dirname, 'src'),
-      '@components': path.resolve(__dirname, 'src/components'),
-      '@hooks': path.resolve(__dirname, 'src/hooks'),
+module.exports = (webpackEnv, argv) => {
+  const isEnvDevelopment = argv.mode === 'development';
+  const isEnvProduction = argv.mode === 'production';
+
+  return {
+    resolve: {
+      extensions: ['.js', '.jsx', '.ts', '.tsx'],
+      alias: {
+        '@': path.resolve(__dirname, 'src'),
+        '@components': path.resolve(__dirname, 'src/components'),
+        '@hooks': path.resolve(__dirname, 'src/hooks'),
+      },
     },
-  },
-  entry: './src/index.tsx',
-  output: {
-    filename: '[name].[chunkhash].js',
-    path: path.resolve(__dirname, 'dist'),
-    publicPath: '/',
-    clean: true,
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(t|j)sx?$/,
-        use: 'swc-loader',
-        exclude: /node_modules/,
-      },
-      {
-        test: /\.s?css$/,
-        use: ['style-loader', 'css-loader', 'postcss-loader', 'sass-loader'],
-      },
-    ],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, 'src/index.html'),
-    }),
-    new CopyWebpackPlugin({
-      patterns: [
+    entry: './src/index.tsx',
+    output: {
+      filename: 'static/js/[name].[contenthash:8].js',
+      path: path.resolve(__dirname, 'build'),
+      publicPath: '/',
+      clean: true,
+    },
+    module: {
+      rules: [
         {
-          from: 'public',
+          test: /\.(t|j)sx?$/,
+          use: 'swc-loader',
+          exclude: /node_modules/,
+        },
+        {
+          test: /\.s?css$/,
+          use: ['style-loader', 'css-loader', 'postcss-loader', 'sass-loader'],
+        },
+        {
+          test: /\.svg$/,
+          use: [
+            {
+              loader: '@svgr/webpack',
+              options: {
+                svgo: false,
+                svgoConfig: {
+                  plugins: [{ removeViewBox: false }],
+                },
+                titleProp: true,
+                ref: true,
+              },
+            },
+          ],
+        },
+        {
+          test: /\.(png|jpg|jpeg|gif)$/,
+          type: 'asset/resource',
         },
       ],
-    }),
-    new ForkTsCheckerWebpackPlugin({
-      typescript: {
-        diagnosticOptions: {
-          semantic: true,
-          syntactic: true,
-        },
-      },
-    }),
-  ],
-  devServer: {
-    port: 3000,
-    hot: true,
-    host: 'localhost',
-    historyApiFallback: true,
-  },
-  optimization: {
-    splitChunks: {
-      chunks: 'all',
     },
-  },
+    plugins: [
+      new HtmlWebpackPlugin(
+        Object.assign(
+          {},
+          {
+            inject: true,
+            template: path.resolve(__dirname, 'public/index.html'),
+          },
+          isEnvProduction
+            ? {
+                minify: {
+                  removeComments: true,
+                  collapseWhitespace: true,
+                  removeRedundantAttributes: true,
+                  useShortDoctype: true,
+                  removeEmptyAttributes: true,
+                  removeStyleLinkTypeAttributes: true,
+                  keepClosingSlash: true,
+                  minifyJS: true,
+                  minifyCSS: true,
+                  minifyURLs: true,
+                },
+              }
+            : undefined
+        )
+      ),
+      new ForkTsCheckerWebpackPlugin({
+        async: isEnvDevelopment,
+        typescript: {
+          diagnosticOptions: {
+            syntactic: true,
+          },
+        },
+        issue: {
+          include: [
+            { file: '../**/src/**/*.{ts,tsx}' },
+            { file: '**/src/**/*.{ts,tsx}' },
+          ],
+          exclude: [
+            { file: '**/src/**/__tests__/**' },
+            { file: '**/src/**/?(*.){spec|test}.*' },
+            { file: '**/src/setupProxy.*' },
+            { file: '**/src/setupTests.*' },
+          ],
+        },
+      }),
+    ],
+    devServer: {
+      port: 3000,
+      hot: true,
+      host: 'localhost',
+      historyApiFallback: true,
+    },
+    optimization: {
+      splitChunks: {
+        chunks: 'all',
+      },
+    },
+  };
 };
